f9ff16fc2eba788a62fac5175b77bcb6d251639b
Islam Kystaubay <kystaubayisl@gmail.com>
2026-05-22 09:54:45 +0500

    Комменты к databse.py

--- app/database.py
+++ app/database.py
@@ -1,3 +1,4 @@
+#Подключение к базе данных и создание сессии для работы с ней
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy.orm import sessionmaker
